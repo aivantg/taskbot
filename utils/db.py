0142fc439f12c42cadc94bf59381304e47d354f1
aivantg <aivant.developer@gmail.com>
2020-06-14 23:23:46 -0500

    add auto rollback

--- utils/db.py
+++ utils/db.py
@@ -9,7 +9,7 @@ if 'DATABASE_URL' in os.environ:
     import psycopg2
     urlparse.uses_netloc.append('postgres')
     url = urlparse.urlparse(os.environ["DATABASE_URL"])
-    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port, autorollback=True)
 else:
     db = SqliteDatabase('bot.db')
 
